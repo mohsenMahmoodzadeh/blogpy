da470f9b0f295cbf8aca76457c8054494516ff05
mohsenMahmoodzadeh <1mohsen.smm@gmail.com>
2021-03-03 14:52:16 +0330

    __str__() added to Category model

--- blog/models.py
+++ blog/models.py
@@ -31,3 +31,6 @@ class Article(models.Model):
 class Category(models.Model):
     title = models.CharField(max_length=128, null=False, blank=False)
     cover = models.FileField(upload_to='files/category_cover/', null=False, blank=False, validators=[validate_file_extension])
+
+    def __str__(self):
+        return self.title
