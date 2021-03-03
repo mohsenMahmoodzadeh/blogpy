9711aec48c26f2d4ec2639626c973bd698d8fd85
mohsenMahmoodzadeh <1mohsen.smm@gmail.com>
2021-03-03 14:18:49 +0330

    Article added to models

--- blog/models.py
+++ blog/models.py
@@ -1,7 +1,18 @@
 from django.db import models
 from django.contrib.auth.models import User
+from ckeditor.fields import RichTextField
+from datetime import datetime
 
 class UserProfile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     avatar = models.FileField(upload_to='files/user_avatar', null=False, blank=False)
     description = models.CharField(max_length=512, null=False, blank=False)
+
+
+class Article(models.Model):
+    title = models.CharField(max_length=128, null=False, blank=False)
+    cover = models.FileField(upload_to='/files/article_cover', null=False, blank=False)
+    content = RichTextField()
+    created_at = models.DateTimeField(default=datetime.now, blank=False)
+    category = models.ForeignKey('Category', on_delete=models.CASCADE)
+    author = models.OneToOneField(UserProfile, on_delete=models.CASCADE)
