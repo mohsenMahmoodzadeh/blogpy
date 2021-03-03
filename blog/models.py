1ff3fc92d1ab199bb42cd432fe512d8bc22e9608
mohsenMahmoodzadeh <1mohsen.smm@gmail.com>
2021-03-03 13:49:37 +0330

    UserProfile added to models

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,7 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class UserProfile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    avatar = models.FileField(upload_to='files/user_avatar', null=False, blank=False)
+    description = models.CharField(max_length=512, null=False, blank=False)
