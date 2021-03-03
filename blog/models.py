48fd1e8fa6f2b1e7b729aeed3e0ba2c69930ef07
mohsenMahmoodzadeh <1mohsen.smm@gmail.com>
2021-03-03 14:54:13 +0330

    __str__() added to Article model

--- blog/models.py
+++ blog/models.py
@@ -27,6 +27,9 @@ class Article(models.Model):
     category = models.ForeignKey('Category', on_delete=models.CASCADE)
     author = models.OneToOneField(UserProfile, on_delete=models.CASCADE)
 
+    def __str__(self):
+        return self.title
+
 
 class Category(models.Model):
     title = models.CharField(max_length=128, null=False, blank=False)
