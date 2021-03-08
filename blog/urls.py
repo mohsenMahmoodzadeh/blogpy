8e02cce55636550aae5bce53cc6ad796f845ca83
mohsenMahmoodzadeh <1mohsen.smm@gmail.com>
2021-03-08 11:17:37 +0330

    AllArticle url added to urlpatterns in urls in blog app

--- blog/urls.py
+++ blog/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     url(r'^$', views.IndexPage.as_view(), name='index'),
     url(r'^contact/$', views.ContactPage.as_view(), name='contact'),
+    url(r'^article/all/$', views.AllArticleAPIView.as_view(), name='all_articles'),
 ]
