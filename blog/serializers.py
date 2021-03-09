d2430da3b07f7352c7dad00db4fe4115c1814f24
mohsenMahmoodzadeh <1mohsen.smm@gmail.com>
2021-03-09 11:45:00 +0330

    AllArticleAPIView serializer implemented

--- blog/serializers.py
+++ blog/serializers.py
@@ -1,6 +1,16 @@
 from rest_framework import serializers
 
 
+class AllArticleSerializer(serializers.Serializer):
+    title = serializers.CharField(required=True, allow_null=False, allow_blank=False, max_length=128)
+    cover = serializers.CharField(required=True, allow_null=False, allow_blank=False, max_length=256)
+    content = serializers.CharField(required=True, allow_null=False, allow_blank=False, max_length=2048)
+    created_at = serializers.DateTimeField(required=True, allow_null=False)
+    category = serializers.CharField(required=True, allow_null=False, allow_blank=False, max_length=128)
+    author = serializers.CharField(required=True, allow_null=False, allow_blank=False, max_length=256)
+    promote = serializers.CharField(required=True, allow_null=False, allow_blank=False, max_length=256)
+
+
 class SingleArticleSerializer(serializers.Serializer):
     title = serializers.CharField(required=True, allow_null=False, allow_blank=False, max_length=128)
     cover = serializers.CharField(required=True, allow_null=False, allow_blank=False, max_length=256)
--- blog/views.py
+++ blog/views.py
@@ -54,16 +54,19 @@ class AllArticleAPIView(APIView):
             data = []
             all_articles = Article.objects.all().order_by('-created_at')[:10]
 
-            for article in all_articles:
-                data.append({
-                    'title': article.title,
-                    'cover': article.cover.url if article.cover else None,
-                    'content': article.content,
-                    'created_at': article.created_at,
-                    'category': article.category.title,
-                    'author': article.author.user.first_name + ' ' + article.author.user.last_name,
-                    'promote': article.promote,
-                })
+            # for article in all_articles:
+            #     data.append({
+            #         'title': article.title,
+            #         'cover': article.cover.url if article.cover else None,
+            #         'content': article.content,
+            #         'created_at': article.created_at,
+            #         'category': article.category.title,
+            #         'author': article.author.user.first_name + ' ' + article.author.user.last_name,
+            #         'promote': article.promote,
+            #     })
+
+            serialized_data = serializers.AllArticleSerializer(all_articles, many=True)
+            data = serialized_data.data
 
             return Response({'data': data}, status=status.HTTP_200_OK)
 
