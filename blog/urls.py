1294aa09f9d74e49f7cf425f4954a81967a83375
mohsenMahmoodzadeh <1mohsen.smm@gmail.com>
2021-03-09 10:02:35 +0330

    SingleArticleAPIView url added to urls in blog app

--- blog/urls.py
+++ blog/urls.py
@@ -4,5 +4,6 @@ from . import views
 urlpatterns = [
     url(r'^$', views.IndexPage.as_view(), name='index'),
     url(r'^contact/$', views.ContactPage.as_view(), name='contact'),
+    url(r'^article/$', views.SingleArticleAPIView.as_view(), name='single_article'),
     url(r'^article/all/$', views.AllArticleAPIView.as_view(), name='all_articles'),
 ]
